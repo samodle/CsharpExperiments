bd968f5ab6d4c3499b96bb67051ff5c2aa1dab15
smodle <odle@simulationdynamics.com>
2016-04-14 17:40:48 -0400

    working list view w/ generic click

--- AND110/XamarinUniversitry/MainActivity.cs
+++ AND110/XamarinUniversitry/MainActivity.cs
@@ -11,7 +11,6 @@ namespace XamarinUniversitry
     [Activity(Label = "XamarinUniversitry", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,12 +19,21 @@ namespace XamarinUniversitry
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            ListView instructorList = FindViewById<ListView>(Resource.Id.instructorListView);
+            instructorList.ItemClick += OnItemClick;
+            instructorList.Adapter = new ArrayAdapter<Instructor>(this, Android.Resource.Layout.SimpleListItem1,InstructorData.Instructors);
         }
+
+        void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var instructor = InstructorData.Instructors[e.Position];
+
+            var dialogue = new AlertDialog.Builder(this);
+            dialogue.SetMessage(instructor.Name);
+            dialogue.SetNeutralButton("OK", delegate { });
+            dialogue.Show();
+        }
+
     }
 }
 
--- AND110/XamarinUniversitry/Resources/Resource.Designer.cs
+++ AND110/XamarinUniversitry/Resources/Resource.Designer.cs
@@ -61,7 +61,7 @@ namespace XamarinUniversitry
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int instructorListView = 2131034112;
 			
 			static Id()
 			{
