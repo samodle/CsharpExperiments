41826c4ab7f52ca2aee3b3211cc4f1d19073171e
Sam Odle <odle@simulationdynamics.com>
2016-04-13 22:05:17 -0400

    changes made during demo w/ inmates

--- AND102_ActivitiesIntents/GroceryList/Resources/Resource.designer.cs
+++ AND102_ActivitiesIntents/GroceryList/Resources/Resource.designer.cs
@@ -24,10 +24,6 @@ namespace GroceryList
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
-		public static void UpdateIdValues()
-		{
-		}
-		
 		public partial class Attribute
 		{
 			
