128e2d1bb05b0dd8ff7e7d6797310c8671a92b1a
Sam Odle <odle@simulationdynamics.com>
2016-04-26 10:10:53 -0400

    working v1 of diary

---
+++ Evolve_SecureLocalData/Diary.PCL/AccountManager.cs
@@ -0,0 +1,58 @@
+﻿using Xamarin.Auth;
+using System.Linq;
+
+namespace Diary.Shared
+{
+	public class AccountManager
+	{
+		const string serviceID 	= "Diary";
+		const string pwKey = "password";
+		const string kmKey = "keymaterial";
+		const string saltKey = "salt";
+
+ 		public bool CreateAndSaveAccount (string username, string password)
+		{
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+				return false;
+
+            AccountStore store = AccountStore.Create();
+            if (GetAccountFromStore(store, username) != null)
+                return false;
+
+            Account account = new Account(username);
+            account.Properties.Add(pwKey, password);
+
+            store.Save(account, serviceID);
+
+			return true;
+		}
+
+
+
+
+        public bool LoginToAccount (string username, string password)
+		{
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+				return false;
+
+            Account account = GetAccount(username);
+              return account != null && account.Properties[pwKey] == password;
+		}
+
+
+
+        public Account GetAccount (string username)
+		{
+			return GetAccountFromStore (AccountStore.Create (), username);
+		}
+
+		Account GetAccountFromStore (AccountStore store, string username)
+		{
+            if (store == null || username == null)
+                return null;
+
+            var accounts = store.FindAccountsForService(serviceID);
+            return accounts.FirstOrDefault(a => a.Username == username);
+        }
+	}
+}
---
+++ Evolve_SecureLocalData/Diary.PCL/Security/CryptoUtilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using PCLCrypto;
+using System.Text;
+
+namespace Diary.Shared
+{
+	public static class CryptoUtilities
+	{
+		const int IVSize = 16;
+
+		public static byte[] GetAES256KeyMaterial ()
+		{
+			return WinRTCrypto.CryptographicBuffer.GenerateRandom(32); 
+		}
+
+		public static byte[] Get256BitSalt ()
+		{
+			return WinRTCrypto.CryptographicBuffer.GenerateRandom(32); 
+		}
+
+		public static byte[] Encrypt (byte[] plainText, byte[] keyMaterial)
+		{
+			//TODO
+			return plainText;
+		}
+
+		public static byte[] Decrypt (byte[] cipherText, byte[] keyMaterial)
+		{
+			//TODO
+			return cipherText;
+		}
+
+		public static byte[] GetHash (byte[] data, byte[] salt)
+		{
+			//TODO
+			return data;
+		}
+
+		public static string ByteArrayToString (byte[] data)
+		{
+			return Encoding.UTF8.GetString (data, 0, data.Length);
+		}
+
+		public static byte[] StringToByteArray (string text)
+		{
+			return Encoding.UTF8.GetBytes (text);
+		}
+	}
+}
